abe804525ce4f3724e7c52f30f18ada487796c50
Luciano Mintrone <luciano@amalgama.co>
2026-01-23 19:01:42 -0300

    Change to Prisma Accelerate

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+function createPrismaClient() {
+  return new PrismaClient().$extends(withAccelerate());
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
